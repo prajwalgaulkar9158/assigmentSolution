89c80bcda5cf5c409d61c76342fd6ddf668dcd96
prajwal gaulkar <prajwalgaulkar78717@gmail.com>
2023-05-11 09:08:09 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prajwalgaulkar78717:bXvJ3nSxWJSUIcpZ@cluster0.vdcbqqv.mongodb.net/test-2", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
